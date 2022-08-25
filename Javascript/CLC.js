c59905da632f4ba9c0c3a6478411b174b2a76bd8
Murat Develi <muratdeveli03@gmail.com>
2022-08-25 22:41:54 +0300

    js

---
+++ Javascript/CLC.js
@@ -0,0 +1,20 @@
+function a()
+{
+    return
+    {
+        name: "Murat"
+    }
+
+}
+
+function b()
+{
+    return
+    {
+        name: "Develi"
+    }
+
+}
+console.log (a());
+console.log (b());
+
