d1876c09c3fbadc40d431f9690a9f852dde58fa9
Murat Develi <muratdeveli03@gmail.com>
2022-08-26 13:10:58 +0300

    jv

---
+++ Javascript/Lecture-45-Part 2-Creating Objects Using Object Literal Syntax/script.js
@@ -0,0 +1,13 @@
+//Object Creation
+var facebook = {
+    name: "Facebook",
+    ceo: {
+      firstName: "Mark",
+      favColor: "blue"
+    },
+    "stock of company": 110
+  };
+  
+  console.log(facebook);
+  console.log(facebook.ceo.firstName);
+  
