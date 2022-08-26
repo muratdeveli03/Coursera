580cf2eadb113b85d834afaecc76f7926a0a664a
Murat Develi <muratdeveli03@gmail.com>
2022-08-26 12:20:52 +0300

    sd

--- Javascript/script.js
+++ Javascript/script.js
@@ -22,9 +22,8 @@ for (var i = 0; i < 10; i++) {
 console.log("sum of 0 through 9 is:" + sum);
 
 function orderChickenWith(sideDish) {
-   sideDish= sideDish || " tomatoe sauce"
-    console.log("Chicken with" + sideDish);
+  sideDish = sideDish || " tomatoe sauce";
+  console.log("Chicken with" + sideDish);
 }
 orderChickenWith(" noodles");
 orderChickenWith();
-
